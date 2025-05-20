a637994d1532d2554edbaae658383743544a20d4
soxziw <eric.xu22719@gmail.com>
2025-05-20 23:50:06 +0000

    fix

--- primary/src/error.rs
+++ primary/src/error.rs
@@ -1,6 +1,6 @@
 // Copyright(C) Facebook, Inc. and its affiliates.
 use crate::primary::Round;
-use crypto::{CryptoError, Digest, PublicKey};
+use crypto::{Digest, PublicKey};
 use store::StoreError;
 use thiserror::Error;
 
@@ -24,8 +24,8 @@ pub type DagResult<T> = Result<T, DagError>;
 
 #[derive(Debug, Error)]
 pub enum DagError {
-    #[error("Invalid signature")]
-    InvalidSignature(#[from] CryptoError),
+    // #[error("Invalid signature")]
+    // InvalidSignature(#[from] CryptoError),
 
     #[error("Storage failure: {0}")]
     StoreError(#[from] StoreError),
